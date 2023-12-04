3a666dd87eca92ef98914676a71a9abafc287cd0
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-12-04 18:07:57 +0400

    add modal

--- src/components/VoterModal.tsx
+++ src/components/VoterModal.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import Dialog from "@mui/material/Dialog";
+import { useFormik } from "formik";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
-function VoterModal() {
-  return (
-    <div>VoterModal</div>
-  )
+interface VoterModalProps {
+  open: boolean;
+  onClose: () => void;
+  onSubmit: (values: { voterId: number; name: string }) => void;
 }
 
-export default VoterModal
+function VoterModal({ open, onClose, onSubmit }: VoterModalProps) {
+  const formik = useFormik({
+    initialValues: {
+      voterId: null,
+      name: "",
+    },
+    onSubmit: (values) => {
+      // onSubmit(values);
+      onClose();
+    },
+  });
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          id="voterId"
+          name="voterId"
+          label="Voter ID"
+          onChange={formik.handleChange}
+          value={formik.values.voterId}
+        />
+        <TextField
+          id="name"
+          name="name"
+          label="Name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Dialog>
+  );
+}
+
+export default VoterModal;
